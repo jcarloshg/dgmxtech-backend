5e151786f79dcb8b6215c8f6c3bbeae8eb69f24b
jcarloshg <carlosj12336@gmail.com>
2024-08-21 08:49:17 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (_req, res) => {
+    res.status(200).send("express with typescript!!" + new Date() + "HOLA")
+})
+
+app.listen(3000, () => {
+    console.log(`App is running but the port: ${3000}`)
+})
