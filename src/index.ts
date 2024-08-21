ff58bf6e54ab7d4e0f46e0b318042e2e3eeaa081
jcarloshg <carlosj12336@gmail.com>
2024-08-21 09:06:27 -0600

    add architecture

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from "express"
+import cookieParser from 'cookie-parser'
+import { corsMiddleware } from "./express/middlewares/cors"
+import { PORT } from "./utils/KEYS_ENVIRONMENT"
+
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
+app.use(corsMiddleware())
 
 app.get('/', (_req, res) => {
     res.status(200).send("express with typescript!!" + new Date())
 })
 
-app.listen(3000, () => {
-    console.log(`App is running but the port: ${3000}`)
+app.listen(PORT, () => {
+    console.log(`App is running but the port: ${PORT}`)
 })
