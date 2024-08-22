c798e9716d21a22e6f03542bcf326ed965d9227f
jcarloshg <carlosj12336@gmail.com>
2024-08-22 15:14:03 -0600

    add origin to cors

--- src/express/middlewares/cors.ts
+++ src/express/middlewares/cors.ts
@@ -3,11 +3,12 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:1234',
-    'https://movies.com',
-    'https://midu.dev'
+    'http://localhost:4200',
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+
+
     origin: (origin, callback) => {
 
         if (!origin) return callback(null, true)
